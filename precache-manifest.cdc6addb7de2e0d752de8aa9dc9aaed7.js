7f196afca8fa71aa06a1aeedbe46c076586e738d
Sudhar J <jaisudharson28@gmail.com>
2020-08-11 23:01:45 +0530

    Updates

---
+++ precache-manifest.cdc6addb7de2e0d752de8aa9dc9aaed7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c336a08cd5480bddf2a11dc97bd0567c",
+    "url": "/singup-react/index.html"
+  },
+  {
+    "revision": "22c9185de523007966c0",
+    "url": "/singup-react/static/js/2.b7b03db8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/singup-react/static/js/2.b7b03db8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7095702f67556ba05a6",
+    "url": "/singup-react/static/js/main.d972cf5f.chunk.js"
+  },
+  {
+    "revision": "17edcd8ca344541cc2f8",
+    "url": "/singup-react/static/js/runtime-main.67e42004.js"
+  }
+]);
